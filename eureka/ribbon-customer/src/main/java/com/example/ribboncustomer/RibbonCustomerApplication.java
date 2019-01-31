53a031e5814258e3253b327c5678bfffaa8d123e
zhangfanchao <32469203@qq.com>
2019-01-31 17:21:59 +0800

    20190131

--- eureka/ribbon-customer/src/main/java/com/example/ribboncustomer/RibbonCustomerApplication.java
+++ eureka/ribbon-customer/src/main/java/com/example/ribboncustomer/RibbonCustomerApplication.java
@@ -1,13 +1,19 @@
 package com.example.ribboncustomer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class RibbonCustomerApplication {
 
@@ -16,6 +22,17 @@ public class RibbonCustomerApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(RibbonCustomerApplication.class, args);
     }
--- eureka/ribbon-customer/src/main/java/com/example/ribboncustomer/RibbonService.java
+++ eureka/ribbon-customer/src/main/java/com/example/ribboncustomer/RibbonService.java
@@ -1,5 +1,6 @@
 package com.example.ribboncustomer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +10,14 @@ public class RibbonService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hi(String name){
+
+     //   return restTemplate.getForObject("https://www.baidu.com/",String.class);
         return restTemplate.getForObject("http://eureka-client-zfc/hello",String.class);
     }
 
+    public String hiError(String name){
+        return "sorry, error!";
+    }
 }
